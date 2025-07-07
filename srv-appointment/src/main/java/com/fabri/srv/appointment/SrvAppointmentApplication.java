63a837d0cf0329e74ea9d771761a952ece04df88
fbrcmmelo <fbrcmmelo@gmail.com>
2025-07-07 18:59:09 -0300

    feat:srv-appointment: spring module created

---
+++ srv-appointment/src/main/java/com/fabri/srv/appointment/SrvAppointmentApplication.java
@@ -0,0 +1,11 @@
+package com.fabri.srv.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvAppointmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SrvAppointmentApplication.class);
+    }
+}
